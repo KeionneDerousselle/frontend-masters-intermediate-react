50958ebdfd5e3153505abd5dfdda53c33425c132
Keionne Derousselle <knnderousselle@gmail.com>
2020-11-30 15:38:26 -0700

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
 import { render } from 'react-dom'
 import SearchParams from './SearchParams'
 import { Link, Router } from '@reach/router'
-import Details from './Details'
 import ThemeContext from './ThemeContext'
+
+// only code split components that are 30kb or larger. 
+// extra code splitting only makes load times longer if you don't need to do it
+const Details = lazy(() => import('./Details'))
 
 const App = () => {
   const themeHook = useState('peru')
@@ -15,10 +18,13 @@ const App = () => {
           <header>
             <Link to="/">Adopt Me!</Link>
           </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense
+            fallback={<h1>Loading...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
